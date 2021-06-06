4e8958a062f30cac25a113a5d2dc3a7dab3fc53c
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-06-06 17:14:25 +0300

    edit

---
+++ src/component/Main.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+
+class Main extends React.Component {
+
+    render(){
+        return(
+            <div>
+                <div>
+                <h2>
+                Unicorn Hollow
+                </h2>
+                    <img src="https://process.filepicker.io/APHE465sSbqvbOIStdwTyz/rotate=deg:exif/resize=fit:crop,height:283,width:472/output=quality:80,compress:true,strip:true,format:jpg/cache=expiry:max/https://cdn.filestackcontent.com/1KfnS1kETJC8bdgBcizm" alt="not work"/>
+
+                <p>The unicorn is a legendary creature that has been described since antiquity as a beast with a single large, pointed, spiraling horn projecting from its forehead. The unicorn was depicted in ancient seals of the Indus Valley Civilization</p>
+                </div>
+                <div>
+                <h2>
+                Unicorn Hollow
+                </h2>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/70/Oftheunicorn.jpg/220px-Oftheunicorn.jpg" alt="not work"/>
+
+                <p>The unicorn is a legendary creature that has been described since antiquity as a beast with a single large, pointed, spiraling horn projecting from its forehead. The unicorn was depicted in ancient seals of the Indus Valley Civilization</p>
+                </div>
+                <div>
+                <h2>
+                Unicorn Hollow
+                </h2>
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Ur-painting.jpg/220px-Ur-painting.jpg" alt="not work"/>
+
+                <p>The unicorn is a legendary creature that has been described since antiquity as a beast with a single large, pointed, spiraling horn projecting from its forehead. The unicorn was depicted in ancient seals of the Indus Valley Civilization</p>
+                </div>
+               
+            </div>
+        )
+    }
+}
+export default Main
