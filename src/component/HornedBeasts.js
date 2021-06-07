f445fd1ac6607b96f4baf8f7ff2a53f582218332
yazan ahmad <yazan-ahmed1999a@outlook.com>
2021-06-07 17:09:53 +0300

    lab02

--- src/component/HornedBeasts.js
+++ src/component/HornedBeasts.js
@@ -1,38 +1,33 @@
 import React from 'react';
 
-
 class HornedBeasts extends React.Component {
 
-    render(){
-        return(
-            <div>
-                <div>
-                <h2>
-                UniWhal 
-                </h2>
-                    <img src= "http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="not work"/>
+  constructor(props){
+    super(props)
 
-                <p>A unicorn and a narwhal nuzzling their horns</p>
-                </div>
-                <div>
-                <h2>
-                Rhino Family
-                </h2>
-                    <img src="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" alt="not work"/>
-
-                <p>Mother (or father) rhino with two babies</p>
-                </div>
-                <div>
-                <h2>
-                Unicorn Head
-                </h2>
-                    <img src="https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg" alt="not work"/>
-
-                <p>Someone wearing a creepy unicorn head mask</p>
-                </div>
-               
-            </div>
-        )
+    this.state = {
+      countVotes : 0 
     }
+  }
+
+ Voting = () => {
+    let numVotes = this.state.countVotes;
+    this.setState({
+        countVotes : numVotes += 1
+    })
+ }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <h2>{this.props.title}</h2>
+          <img onClick={this.Voting} src={this.props.imageUrl} alt={this.props.keyword}></img>
+          <p>Description : {this.props.description}</p>
+          <p>Votes : {this.state.countVotes}</p>
+        </div>
+      </div>
+    );
+  }
 }
-export default HornedBeasts
+export default HornedBeasts;
